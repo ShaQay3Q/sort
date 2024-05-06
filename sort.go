2680c0719ecd7744141a6e89d52871e901079014
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-06 19:01:18 +0200

    insertion sort with go

--- sort.go
+++ sort.go
@@ -1,1 +1,13 @@
 package sort
+
+func main() {
+
+}
+
+func swap(initArr []int) []int {
+	return []int{1, 2}
+}
+
+func sortInsertion(intitArr []int) []int {
+	return intitArr
+}
--- sort_test.go
+++ sort_test.go
@@ -1,1 +1,28 @@
 package sort
+
+import (
+	_ "image/png"
+
+	//"runtime/trace"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSwap(t *testing.T) {
+	initArr := []int{2, 1}
+	var swapArr []int
+
+	swapArr = swap(initArr)
+
+	require.Equal(t, swapArr, []int{1, 2})
+
+}
+
+func TestSortInsertion(t *testing.T) {
+	intitArr := []int{1, 2}
+	var sortArr []int
+
+	sortArr = sortInsertion(intitArr)
+	require.Equal(t, sortArr, []int{1, 2})
+}
